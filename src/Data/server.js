59fc03f84f8e9859433815ae116b00e604f49e4f
Decooding <salauat.160502@gmail.com>
2023-06-06 00:19:17 +0600

    123

--- src/Data/server.js
+++ src/Data/server.js
@@ -22,8 +22,9 @@ connection.connect((err) => {
   console.log('Connected to the database.');
 
   // Запрос
-  rl.question('Введите SQL запрос: ', (query) => {
+  rl.question('Введите SQL запрос (update,adduser,addrest): ', (query) => {
     if (query === 'update') {
+      // код обновления данных
       connection.query('SELECT * FROM restouran', (error, results) => {
         if (error) {
           console.error('Error executing the query:', error);
@@ -32,8 +33,6 @@ connection.connect((err) => {
         const data = JSON.stringify(results);
         const code = `const data = ${data};\n\nexport default data;`;
         fs.writeFileSync('./data.js', code, 'utf-8');
-
-        console.log('Данные обновлены успешно.');
       });
 
       connection.query('SELECT * FROM review', (error, results) => {
@@ -56,9 +55,59 @@ connection.connect((err) => {
 
         fs.writeFileSync('./users.js', code, 'utf-8');
       });
+
+      console.log('Данные обновлены успешно.');
+
     } else if (query === 'close') {
       connection.end();
       rl.close();
+    } else if (query === 'adduser') {
+      // Запрос на добавление пользователя
+      rl.question('Введите имя пользователя: ', (username) => {
+        rl.question('Введите адрес электронной почты: ', (email) => {
+          rl.question('Введите пароль: ', (password) => {
+            const img_url = 'https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/user-male-circle-blue-1024.png';
+            const addUserQuery = `INSERT INTO user (name, email, password, img_url) VALUES ('${username}', '${email}', '${password}', '${img_url}')`;
+
+            connection.query(addUserQuery, (error, results) => {
+              if (error) {
+                console.error('Ошибка при выполнении запроса:', error);
+                return;
+              }
+              console.log('Пользователь успешно добавлен в базу данных.');
+            });
+
+            rl.close();
+          });
+        });
+      });
+
+    } else if (query === 'addrest') {
+      // Запрос на добавление заведения
+      rl.question('Введите тип заведения: ', (type) => {
+        rl.question('Введите название заведения: ', (title) => {
+          rl.question('Введите адрес заведения: ', (address) => {
+            const kuh = 'Европейская';
+            const time = 'ПН-ВС 12:00–00:00';
+            const image = 'https://astana.restolife.kz/upload/information_system_30/2/5/9/item_25932/small_information_items_property_33529.jpg';
+            const text = 'Приглашаем вас отведать изысканные блюда и насладиться атмосферой уюта в нашем ресторане/баре/кафе. Наше меню порадует вас разнообразием кулинарных шедевров, приготовленных из свежих и качественных ингредиентов. Наш внимательный персонал готов сделать ваше посещение незабываемым и удовлетворить самые изысканные вкусы. Приходите и насладитесь неповторимым гастрономическим опытом в нашем заведении.';
+            const rating = '⭐️⭐️⭐️⭐️⭐️';
+    
+            const addRestaurantQuery = `INSERT INTO restouran (type, title, kuh, time, address, image, text, Rating) VALUES ('${type}', '${title}', '${kuh}', '${time}', '${address}', '${image}', '${text}', '${rating}')`;
+    
+            connection.query(addRestaurantQuery, (error, results) => {
+              if (error) {
+                console.error('Ошибка при выполнении запроса:', error);
+                return;
+              }
+              console.log('Заведение успешно добавлено в базу данных.');
+            });
+
+          rl.close();
+        });
+      });
+    });
+
     } else {
       // Выполнение обычного запроса
       connection.query(query, (error, results) => {
@@ -69,9 +118,7 @@ connection.connect((err) => {
         console.log('Результаты запроса:');
         console.log(results);
       });
+      rl.close();
     }
-
-    // Закрытие readline после выполнения запроса
-    rl.close();
   });
 });
